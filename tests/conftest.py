54d70eb54064597b4a2bacb251d22287fe07b37e
James Hiebert <hiebert@uvic.ca>
2013-07-31 08:58:56 -0700

    Modified test fixtures to use egg-safe dsn paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,11 @@ from sqlalchemy.orm import sessionmaker
 
 @pytest.fixture(scope="module")
 def test_session():
-    engine = create_engine('sqlite+pysqlite:///{0}'.format(pycds.test_dsn))
+    engine = create_engine(pycds.test_dsn)
     engine.echo = True
     Session = sessionmaker(bind=engine)
     return Session()
 
 @pytest.fixture(scope="module")
 def conn_params():
-    return 'sqlite+pysqlite:///{0}'.format(pycds.test_dsn)
+    return pycds.test_dsn
