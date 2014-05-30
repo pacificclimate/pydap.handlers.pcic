ce158198a9eb35cf018b4a44070fb48e0b8803b0
Basil Veerman <bveerman@uvic.ca>
2014-05-29 17:24:40 -0700

    Add spatialte check to setuptool TestCommand

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import sys
 import string
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
+from pkg_resources import resource_filename
+import ctypes
+import warnings
+
+__version__ = (0, 0, 4)
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -11,10 +16,36 @@ class PyTest(TestCommand):
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
         import pytest
+        if not check_for_spatialite():
+            raise Exception("Cannot run without libspatialite, which is not installed")
         errno = pytest.main(self.test_args)
         sys.exit(errno)                                                                        
 
-__version__ = (0, 0, 4)
+def check_for_spatialite():
+    try:
+        ctypes.cdll.LoadLibrary('libspatialite.so')
+    except OSError:
+        warnings.warn("libspatialite must be installed if you want to run the tests")
+        return False
+
+    from sqlalchemy import create_engine
+
+    msg = '''Unfortunately, for using sqlite for testing geographic functionality, you need to manually install pysqlite2 according to the instructions here: http://www.geoalchemy.org/usagenotes.html#notes-for-spatialite
+Otherwise, you will not be able to run any of the unit tests for this package for any packages which depend upon it.'''
+
+    try:
+        from pysqlite2 import dbapi2
+    except ImportError:
+        warnings.warn('pysqlite2 package is not installed.\n' + msg)
+        return False
+
+    engine = create_engine('sqlite:///{0}'.format('pycds/data/crmp.sqlite'), module=dbapi2, echo=True)
+    con = engine.raw_connection().connection
+    if not hasattr(con, 'enable_load_extension'):
+        warnings.warn('The pysqlite2 package has been built without extension loading support.\n' + msg)
+        return False
+    else:
+        return True
 
 setup(
     name="pydap.handlers.pcic",
